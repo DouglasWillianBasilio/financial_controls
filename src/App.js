3926059fc40837892065dc5edd5b1187d52faab2
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-04-07 17:55:27 -0300

    error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
 import Header from "./componentes/Header";
-import Resume from "./componentes/Resume";
+
 
 
 const App = () => {
     return (
     <>
         <Header />
-        <Resume />
         <GlobalStyle />
     </>
     )
