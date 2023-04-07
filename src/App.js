7c50ca320cf8812bb48477d7d1e4d48ee085c1cc
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-04-07 17:17:23 -0300

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
-import Headers from "./componets/header";
+import Headers from "./componetes/header/Header";
+import Resume from "./componetes/header/Resume";
 const App = () => {
     return (
     <>
         <Headers />
+        <Resume />
         <GlobalStyle />
     </>
     )
