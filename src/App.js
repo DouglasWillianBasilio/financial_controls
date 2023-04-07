31ca5ddcfe465abbfdcf3f45a1d90c145340340f
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-04-07 17:41:56 -0300

    error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
-import Headers from "./componetes/header/Header";
-import Resume from "./componetes/header/Resume";
+import Header from "./componentes/Header";
+import Resume from "./componentes/Resume";
+
+
 const App = () => {
     return (
     <>
-        <Headers />
+        <Header />
         <Resume />
         <GlobalStyle />
     </>
---
+++ src/componentes/Resume/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as C from "./styles";
+const Resume = () => {
+    return (
+        <C.Container>
+            teste
+        </C.Container>
+    );
+}
+
+export default Resume;
